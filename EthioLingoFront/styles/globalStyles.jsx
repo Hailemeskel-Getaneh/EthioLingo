eaa2da0184e6ff9ee5fa3898236335491e0f6d7b
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-07 01:49:54 +0300

    global styles updated

--- EthioLingoFront/styles/globalStyles.jsx
+++ EthioLingoFront/styles/globalStyles.jsx
@@ -1,9 +1,7 @@
-// /EthioLingoFront/styles/globalStyles.js
 
 import { StyleSheet } from 'react-native';
 
 export const colors = {
-  // Main theme colors
   primaryBackground: '#313574',
   primaryText: '#f0f2f5', 
   screenBackground: '#fafafb', 
@@ -11,7 +9,6 @@ export const colors = {
   listBarBackground: '#ffffff', 
   listBarText: '#131313', 
 
-  // Additional colors for icons and accents
   accent1: '#fbfaff',
   accent2: '#70a595',
   accent3: '#959163',
@@ -20,17 +17,15 @@ export const colors = {
 };
 
 export const globalStyles = StyleSheet.create({
-  // Base styles for screens
   screenContainer: {
     flex: 1,
     backgroundColor: colors.screenBackground,
   },
   screenText: {
     color: colors.screenText,
-    fontSize: 16,
+    fontSize: 19,
   },
 
-  // Styles for buttons
   primaryButton: {
     backgroundColor: colors.primaryBackground,
     paddingVertical: 12,
@@ -41,11 +36,10 @@ export const globalStyles = StyleSheet.create({
   },
   primaryButtonText: {
     color: colors.primaryText,
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: '600',
   },
 
-  // Styles for small horizontal bars (e.g., languages list)
   
   listBar: {
     backgroundColor: colors.listBarBackground,
@@ -56,11 +50,10 @@ export const globalStyles = StyleSheet.create({
   },
   listBarText: {
     color: colors.listBarText,
-    fontSize: 14,
+    fontSize: 23,
     fontWeight: '500',
   },
 
-  // Styles for icons containers (you can use these as background for icons)
   iconContainer: {
     backgroundColor: colors.accent2, 
     padding: 8,
