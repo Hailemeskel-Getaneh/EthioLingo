a936b5759d655b97f752ed1480564d616a27d91c
MierafA12 <mierafabebe12@gmail.com>
2025-03-06 19:07:04 +0300

    ResetPasswordPage

---
+++ EthioLingoFront/screens/Auth/ResetPassword.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { TouchableOpacity, View, Text, TextInput } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
+import { ResetButton2 } from '../../components/Buttons/onbordingButtons';
+
+const ResetPassword = () => {
+  const navigation = useNavigation();
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
+
+  return (
+    <View className="flex-1 bg-white p-6">
+      <TouchableOpacity onPress={() => navigation.goBack('Verfication')}>
+        <View className="flex-row items-center mt-6">
+          <Ionicons name="arrow-back" size={24} color="black" />
+          <Text className="text-xl font-bold text-[#313574] ml-2">Back</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View className="w-full max-w-sm mt-40">
+        <Text className="text-2xl font-bold text-[#313574] text-center">Reset Password</Text>
+        <Text className="text-gray-600 mt-2 text-center">Please enter the new password</Text>
+      </View>
+
+    
+      <View>
+        <Text className="text-1xl font-bold text-[#313574] mt-5">New Password</Text>
+        <View className="relative">
+          <TextInput
+            className="w-full p-4 border border-gray-300 rounded-lg text-lg mt-5"
+            placeholder="Enter your new password"
+            value={Password}
+            onChangeText={setPassword}
+            secureTextEntry={!isPasswordVisible}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 10,
+              top: '50%',
+              transform: [{ translateY: -12 }],
+            }}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+          >
+            <Ionicons
+              name={isPasswordVisible ? 'eye-off' : 'eye'}
+              size={24}
+              color="#313574"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <Text className="text-1xl font-bold text-[#313574] mt-5">Confirm Password</Text>
+        <View className="relative">
+          <TextInput
+            className="w-full p-4 border border-gray-300 rounded-lg text-lg mt-5"
+            placeholder="Confirm Password"
+            value={ConfirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry={!isConfirmPasswordVisible} 
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 10,
+              top: '50%',
+              transform: [{ translateY: -12 }],
+            }}
+            onPress={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
+          >
+            <Ionicons
+              name={isConfirmPasswordVisible ? 'eye-off' : 'eye'}
+              size={24}
+              color="#313574"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+       <View className="mt-5 w-full" >
+         <ResetButton2 onPress={()=>navigation.navigate('Complate')}/>
+       </View>
+      
+    </View>
+  );
+};
+
+export default ResetPassword;
