5981ac5d5f5cddb28bd311f3997ed573e7ea6be7
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-09 11:07:58 +0300

    speaking screen

--- EthioLingoFront/tailwind.config.js
+++ EthioLingoFront/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Include all files in all directories
     "./**/*.{js,jsx,ts,tsx}",
-    // Explicitly exclude node_modules
     "!**/node_modules/**"
   ],
   presets: [require("nativewind/preset")],
