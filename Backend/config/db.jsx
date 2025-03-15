f0a1266ec9410b6becbd60cf532271f71c4aa52d
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-15 19:43:39 +0300

    basic packages installed and database connected

--- Backend/config/db.jsx
+++ Backend/config/db.jsx
@@ -1,1 +1,15 @@
+import mongoose from 'mongoose'
 
+const DatabaseConnection = async () => {
+    try{
+
+     mongoose.connect(process.env.MONGO_URL)
+    console.log('Database connected successfully')
+
+    }
+    catch(error){
+        console.log(`Error: ${error }`)
+    }
+}
+
+export default DatabaseConnection
