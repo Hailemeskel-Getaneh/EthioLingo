87207de31e3915819df19041967193eb102c7cc5
MierafA12 <mierafabebe12@gmail.com>
2025-03-16 21:41:45 +0300

    env file in the front

--- EthioLingoFront/babel.config.js
+++ EthioLingoFront/babel.config.js
@@ -1,13 +1,21 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module:react-native-dotenv", // Use "module:" prefix and options
+        {
+          moduleName: "@env", // How you'll import env variables (e.g., import { API_URL } from '@env')
+          path: ".env",       // Path to your .env file
+          safe: false,        // Set to true if you want to enforce defined variables
+          allowUndefined: true, // Allow undefined variables
+        },
       ],
-      plugins: [
-        // Required for expo-router
-        'react-native-reanimated/plugin',
-      ]
-    };
+      "react-native-reanimated/plugin", // Required for expo-router, keep this last
+    ],
   };
+};
