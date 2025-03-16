ef8011ae29f802a62e3f7949707e0bb257cae8a4
MierafA12 <mierafabebe12@gmail.com>
2025-03-16 21:43:34 +0300

    userprofileroute

---
+++ Backend/routes/userProfileRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import {getUserProfile} from '../controllers/userProfileController.js';
+import authMiddleware from'../middleware/authMiddleware.js'
+
+const router=  express.Router();
+
+router.get('/',authMiddleware,getUserProfile)
+
+export default router;
