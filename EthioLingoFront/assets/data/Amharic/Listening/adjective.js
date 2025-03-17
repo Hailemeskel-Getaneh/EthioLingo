364e4bba5447acdcaa9f58409a3e489f5d26d468
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-17 22:55:57 +0300

    updating the data storing format so that it can be uploaded to cloudinary and stored easil to the database

--- EthioLingoFront/assets/data/Amharic/Listening/adjective.js
+++ EthioLingoFront/assets/data/Amharic/Listening/adjective.js
@@ -1,20 +1,17 @@
 export default {
-    title: '7-Adjective',
-    audioFiles: [
-      {
-      source: require('../../../audio/Record033.mp3'),
-
-        correctText: 'Big',
-        correctOption: 'ትልቅ',
-        options: ['ትልቅ', 'ንጹህ', 'ተኛ', 'ደካማ'],
-      },
-      {
-      source: require('../../../audio/Record034.mp3'),
-
-
-        correctText: 'Small',
-        correctOption: 'ተኛ',
-        options: ['ትልቅ', 'ንጹህ', 'ተኛ', 'ደካማ'],
-      },
-    ],
-  };
+  title: '7-Adjective',
+  audioFiles: [
+    {
+      source: 'Record033.mp3',
+      correctText: 'Big',
+      correctOption: 'ትልቅ',
+      options: ['ትልቅ', 'ንጹህ', 'ተኛ', 'ደካማ'],
+    },
+    {
+      source: 'Record034.mp3',
+      correctText: 'Small',
+      correctOption: 'ተኛ',
+      options: ['ትልቅ', 'ንጹህ', 'ተኛ', 'ደካማ'],
+    },
+  ],
+};
