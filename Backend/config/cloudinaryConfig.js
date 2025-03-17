7f815746e041d153b152f012061f4fd49d8837a6
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-17 22:51:54 +0300

    configering cloudinary

---
+++ Backend/config/cloudinaryConfig.js
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: 'dg6zm0aeo',
+  api_key:'165363315352217',
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+export default cloudinary;
