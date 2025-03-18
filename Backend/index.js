4d44bee1ad81e2a64a2a2293195f3242c036a2ab
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-18 19:30:11 +0300

    audio uploding routes updated

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,8 @@ const PORT = process.env.PORT || 5000;
 DatabaseConnection();
 
 app.use('/api/lessons', lessonRoutes);
-// app.use('/api/audio', audioUploadRoutes);  uncomment this if you want to upload files to cloudinary and also store data to the database
+// app.use('/api/audio', audioUploadRoutes);  // uncomment this if you want to upload files to cloudinary and also store data to the database
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
