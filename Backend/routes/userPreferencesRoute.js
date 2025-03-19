1f11d56aa4584bb9f92774216063908e5be96b8c
MierafA12 <mierafabebe12@gmail.com>
2025-03-19 20:29:07 +0300

    build user preference

---
+++ Backend/routes/userPreferencesRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import {getLanguages} from '../controllers/languageController.js'
+
+const router=express.Router();
+
+router.get('/', getLanguages); 
+
+
+export default router;
