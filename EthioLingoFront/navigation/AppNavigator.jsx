5e403cf84a5f447698e83afa602cc82b5448b470
MierafA12 <mierafabebe12@gmail.com>
2025-04-08 23:10:15 +0300

    update edit profile functionality

--- EthioLingoFront/navigation/AppNavigator.jsx
+++ EthioLingoFront/navigation/AppNavigator.jsx
@@ -31,6 +31,7 @@ import SettingsScreen from '../screens/Profile/SettingsScreen.jsx';
 import privacyPolicyScreen from '../screens/Settings/privacyPolicyScreen.jsx';
 import feedbackScreen from '../screens/Settings/feedbackScreen.jsx';
 import { ThemeProvider } from '../contexts/themeContext.jsx';
+import { UserProvider } from '../contexts/UserProfileContext.jsx';
 
 // Placeholder screens
 function ProgressScreen() {
@@ -84,8 +85,9 @@ function LessonDrawerNavigator() {
 
 function AppNavigator() {
   return (
+    <UserProvider>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LanguageSelectionScreen">
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="GetStartedScreen" component={GetStartedScreen} options={{ headerShown: false }} />
         <Stack.Screen name="GreetingScreen" component={GreetingScreen} options={{ headerShown: false }} />
@@ -110,11 +112,9 @@ function AppNavigator() {
         <Stack.Screen name="SettingsScreen" component={SettingsScreen} options={{ headerShown: false }} />
         <Stack.Screen name="privacyPolicyScreen" component={privacyPolicyScreen} options={{ headerShown: false }} />
         <Stack.Screen name="feedbackScreen" component={feedbackScreen} options={{ headerShown: false }} />
-
       </Stack.Navigator>
-
     </NavigationContainer>
-
+    </UserProvider>
   );
 }
 
