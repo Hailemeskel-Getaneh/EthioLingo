ddface4bb938b20b64d605b0cdf280b995a3f29f
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-24 19:30:00 +0300

    Add haptic touch feedback utility

---
+++ EthioLingoFront/utils/haptics.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Haptic Feedback Helper
+ */
+export const triggerHaptic = (type = 'light') => {
+  try {
+    console.log(`[Haptics] Triggered ${type} feedback`);
+  } catch (e) {
+    // Ignore fallback
+  }
+};
