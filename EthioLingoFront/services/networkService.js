91a3f96541692f72a1a86809b31926c9d9c437d0
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-24 13:40:00 +0300

    Add network connectivity service module

---
+++ EthioLingoFront/services/networkService.js
@@ -0,0 +1,9 @@
+﻿/**
+ * Network Connectivity Service
+ */
+export const NetworkService = {
+  isOnline: true,
+  checkConnection: async () => {
+    return true; // Simplified status checker
+  },
+};
