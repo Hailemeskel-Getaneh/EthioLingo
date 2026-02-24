c6852b2c214c3c87d10f2f64fa5b8089bf0d5790
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-24 08:45:00 +0300

    Add Amharic digits conversion and string utilities

---
+++ EthioLingoFront/utils/languageUtils.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Ethiopian Language & Character Utilities
+ */
+export const toAmharicDigits = (num) => {
+  const amharicDigits = ['á‹', 'á©', 'áª', 'á«', 'á¬', 'á­', 'á®', 'á¯', 'á°', 'á±'];
+  return String(num)
+    .split('')
+    .map((digit) => (isNaN(digit) ? digit : amharicDigits[parseInt(digit, 10)] || digit))
+    .join('');
+};
+
+export const capitalizeWord = (str) => {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
