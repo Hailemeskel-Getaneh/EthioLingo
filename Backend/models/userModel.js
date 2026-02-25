0e1263ab5e666a25523e4ee3c6c9a3465d2793ae
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-25 21:15:00 +0300

    Add user data model helper methods in Backend

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,41 +1,11 @@
-import mongoose from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
+﻿const users = [];
 
+const findUserByEmail = (email) => users.find((u) => u.email === email);
 
-const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    default: () => uuidv4(),
-    primaryKey: true,
-  },
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  verificationCode: {
-    type: String,
-    default: "",
-  },
-  verificationCodeCreatedAt: {
-    type: Date,
-  },
-  isFirstLogin: { 
-    type: Boolean, 
-    default: true 
-  },
-}, {
-  timestamps: true,
-});
+const createUser = (userData) => {
+  const newUser = { id: users.length + 1, ...userData, streak: 0, totalXp: 0 };
+  users.push(newUser);
+  return newUser;
+};
 
-const userModel = mongoose.model("users",userSchema); 
-export default userModel;
+module.exports = { findUserByEmail, createUser };
