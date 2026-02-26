97b8d6165790221c1e11af6e1e4538a626d853b7
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-26 09:00:00 +0300

    Define reusable header navigation styling options

---
+++ EthioLingoFront/navigation/navigationOptions.js
@@ -0,0 +1,12 @@
+﻿import { Colors } from '../styles/colors';
+
+export const defaultScreenOptions = {
+  headerStyle: {
+    backgroundColor: Colors.surface,
+  },
+  headerTintColor: Colors.textPrimary,
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerBackTitleVisible: false,
+};
