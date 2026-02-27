39890a2660af8d979dcab9645e6bb4a69f867cfa
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-27 16:00:00 +0300

    Create LeaderboardScreen view skeleton

---
+++ EthioLingoFront/screens/LeaderboardScreen.jsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { CustomText } from '../components/CustomText';
+
+export const LeaderboardScreen = () => {
+  return (
+    <View style={styles.container}>
+      <CustomText variant="title">Global Leaderboard</CustomText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16 },
+});
