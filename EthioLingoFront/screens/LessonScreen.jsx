01325b3ebdf3967ace54a172446ea27b1ff8bc3a
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-02-27 10:30:00 +0300

    Implement LessonScreen view with progress bar indicator

---
+++ EthioLingoFront/screens/LessonScreen.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { CustomText } from '../components/CustomText';
+import { ProgressBar } from '../components/ProgressBar';
+
+export const LessonScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ProgressBar progress={40} />
+      <CustomText variant="title" style={{ marginTop: 20 }}>Lesson: Amharic Greetings</CustomText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, backgroundColor: '#FFF' },
+});
