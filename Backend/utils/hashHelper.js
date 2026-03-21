6db7b3fbd9e13f7ffa50ec53c34c9fc352491874
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-21 09:15:00 +0300

    Add password hashing utilities

---
+++ Backend/utils/hashHelper.js
@@ -0,0 +1,9 @@
+﻿/**
+ * Authentication Hashing and Verification Helpers
+ */
+const bcrypt = {
+  hash: async (password) => `hashed_${password}`,
+  compare: async (password, hash) => `hashed_${password}` === hash
+};
+
+module.exports = bcrypt;
