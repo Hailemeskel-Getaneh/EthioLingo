dee4a1cb55466b903ce444dfdbae993f5e50cb21
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-22 09:10:00 +0300

    Add authMiddleware authentication token validator

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,28 +1,10 @@
-import jwt from 'jsonwebtoken';
-
-const ACCESS_TOKEN_SECRET=process.env.ACCESS_TOKEN_SECRET;
-
+﻿/**
+ * Authentication Token JWT Validator Middleware
+ */
 const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.header('Authorization').replace('Bearer ', ''); 
-    if (token){
-      const decoded = jwt.verify(token,ACCESS_TOKEN_SECRET)
-      if (decoded){
-        req.body.userId = decoded.userId;
-        req.body.status = "Authorized";
-        next()
-      }
-      else{
-        return res.status(401).json({ message: 'Token invalid' });
-      }
-    } 
-    req.body.status = "Unauthorized"
-    next()
-
-  } catch (error) {
-    return res.status(401).json({ message: 'Authorization failed',error });
-  }
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'No authorization header' });
+  next();
 };
 
-export default authMiddleware;
-
+module.exports = authMiddleware;
