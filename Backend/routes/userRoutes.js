76412c1690422f9076a0594c7edeb156f15feb71
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-26 09:20:00 +0300

    Add userRoutes profile endpoint definition

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,7 @@
+﻿const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.get('/profile', authMiddleware, (req, res) => res.json({ profile: {} }));
+
+module.exports = router;
