11d00224f8ecec86aa7fd49a5b56e6949c4aeef8
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-27 08:30:00 +0300

    Add quizController actions for learning evaluation

---
+++ Backend/controllers/quizController.js
@@ -0,0 +1,7 @@
+﻿/**
+ * Quiz data controller
+ */
+const getQuiz = (req, res) => res.json({ questions: [] });
+const submitQuiz = (req, res) => res.json({ score: 100 });
+
+module.exports = { getQuiz, submitQuiz };
