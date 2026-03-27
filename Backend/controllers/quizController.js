9bc97cfc22357753735c187e8743a60dfb904031
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-27 09:45:00 +0300

    Enhance quizController scoring rules pass 1

--- Backend/controllers/quizController.js
+++ Backend/controllers/quizController.js
@@ -5,3 +5,4 @@ const getQuiz = (req, res) => res.json({ questions: [] });
 const submitQuiz = (req, res) => res.json({ score: 100 });
 
 module.exports = { getQuiz, submitQuiz };
+// Quiz scoring adjustment rule 1
