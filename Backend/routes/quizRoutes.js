9ada44b32cdae371e6707ab960b84ef00f7a53d2
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-28 09:30:00 +0300

    Add quiz routes mapping endpoints

---
+++ Backend/routes/quizRoutes.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const router = express.Router();
+const { getQuiz, submitQuiz } = require('../controllers/quizController');
+
+router.get('/lessons/:id/quiz', getQuiz);
+router.post('/lessons/:id/quiz', submitQuiz);
+
+module.exports = router;
