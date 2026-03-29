907bdcbd71a0e6c8220fcb3c603d0806c6ec0c75
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-03-29 09:20:00 +0300

    Create frontend InputContainer text field wrapper component

---
+++ EthioLingoFront/components/InputContainer.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+export const InputContainer = ({ value, onChangeText, placeholder, secureTextEntry }) => {
+  return (
+    <View style={styles.box}>
+      <TextInput
+        style={styles.text}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: { borderBottomWidth: 1, paddingVertical: 8 },
+  text: { fontSize: 16 }
+});
