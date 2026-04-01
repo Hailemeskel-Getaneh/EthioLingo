81f160d0abffb2d8e96242477e336b6e48f86eb0
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-04-01 16:35:00 +0300

    Enhance LoginScreen layout styles pass 7

--- EthioLingoFront/screens/LoginScreen.jsx
+++ EthioLingoFront/screens/LoginScreen.jsx
@@ -28,3 +28,4 @@ const styles = StyleSheet.create({ box: { flex: 1, padding: 20 } });
 // Login screen enhancement update 4
 // Login screen enhancement update 5
 // Login screen enhancement update 6
+// Login screen enhancement update 7
